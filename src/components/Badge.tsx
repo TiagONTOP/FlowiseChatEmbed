98f6c75ec88dface9e6c44783a60017c539095e3
TiagONTOP <ftiago125@gmail.com>
2024-08-09 16:38:05 +0200

    powered update

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -51,14 +51,14 @@ export const Badge = (props: Props) => {
           {props.footer?.text ?? 'Powered by'}
           <a
             ref={liteBadge}
-            href={props.footer?.companyLink ?? 'https://flowiseai.com'}
+            href={props.footer?.companyLink ?? 'https://www.linkedin.com/in/tiago-ferreira-da-silva-0b6757257/'}
             target="_blank"
             rel="noopener noreferrer"
             class="lite-badge"
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Flowise'}</span>
+            <span>&nbsp;{props.footer?.company ?? 'Tiago'}</span>
           </a>
         </span>
       </Show>
